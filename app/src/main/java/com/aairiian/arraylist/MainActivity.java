6f5aa27f682aff3fc325fac4b0285f3e2abcb13a
a.anait <airiian.anait@depositphotos.com>
2018-10-18 18:19:51 +0300

    Get date into field

--- app/src/main/java/com/aairiian/arraylist/MainActivity.java
+++ app/src/main/java/com/aairiian/arraylist/MainActivity.java
@@ -1,13 +1,57 @@
 package com.aairiian.arraylist;
 
+import android.app.DatePickerDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
+    EditText inputName;
+    EditText inputDate;
+    Button button;
+    Calendar calendar;
+    DatePickerDialog datePickerDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView =(TextView) findViewById(R.id.text_view);
+        button=(Button) findViewById(R.id.button_array);
+        inputName=(EditText) findViewById(R.id.input_name);
+        inputDate=(EditText) findViewById(R.id.button_get_date);
+
+        inputDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                calendar = Calendar.getInstance();
+                int day = calendar.get(Calendar.DAY_OF_MONTH);
+                int month = calendar.get(Calendar.MONTH);
+                int year = calendar.get(Calendar.YEAR);
+
+                datePickerDialog = new DatePickerDialog(MainActivity.this, new DatePickerDialog.OnDateSetListener() {
+
+
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+
+                        inputDate.setText(dayOfMonth + "." + (month+1) + "." + year);
+
+                    }
+                }, day, month, year);
+
+                datePickerDialog.show();
+            }
+        });
     }
 }
